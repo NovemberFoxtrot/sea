51e940ce515d8b6ec6bc6c03ddd50e30357cdc48
NovemberFoxtrot <november@fox.trot>
2014-05-23 02:40:05 -0700

    dff

--- main.c
+++ main.c
@@ -1,48 +1,74 @@
-#include "stk.h"
+#include <stdio.h>
 #include "dbg.h"
 
-const char *MY_NAME= "Mind Eraser";
+#define MAX_DATA 100
 
-void scope_demo(int count) {
-	log_info("count is: %d", count);
+typedef enum EyeColor {
+	BLUE_EYES,
+	GREEN_EYES,
+	BROWN_EYES,
+	BLACK_EYES,
+	OTHER_EYES
+} EyeColor;
 
-	if(count > 100) {
-		int count = 100; // bugs
-		
-		log_info("count in this scope is: %d", count);
+const char *EYE_COLOR_NAMES[] = { "Blue", "Green", "Brown", "Black", "Other" };
+
+typedef struct Person
+{
+	int age;
+	char first_name[MAX_DATA];
+	char last_name[MAX_DATA];
+	EyeColor eyes;
+	float income;
+} Person;
+
+int main(int argc, char **argv)
+{
+	Person you = { .age = 0 };
+
+	int i = 0;
+	char *in = NULL;
+
+	printf("What is your FIRST NAME? ");
+	in = fgets(you.first_name, MAX_DATA - 1, stdin);
+	check(in != NULL, "Failed to read first name.");
+
+	printf("What's your Last Name? ");
+	in = fgets(you.last_name, MAX_DATA - 1, stdin);
+	check(in != NULL, "Failed to read last name.");
+
+	printf("How old are you? ");
+	int rc = fscanf(stdin, "%d", &you.age);
+	check(rc > 0, "You have to enter a number.");
+
+	printf("What color are your eyes:\n");
+	for (i = 0; i <= OTHER_EYES; i++) {
+		printf("%d) %s\n", i + 1, EYE_COLOR_NAMES[i]);
 	}
+	printf("> ");
 
-	log_info("count is at exit: %d", count);
+	int eyes = -1;
+	rc = fscanf(stdin, "%d", &eyes);
+	check(rc > 0, "You have to enter a number.");
 
-	count = 3000;
+	you.eyes = eyes - 1;
+	check(you.eyes <= OTHER_EYES && you.eyes >= 0,
+	      "Do it right, that's not an option.");
 
-	log_info("count after assign: %d", count);
-}
+	printf("How much do you make an hour? ");
+	rc = fscanf(stdin, "%f", &you.income);
+	check(rc > 0, "Enter a floating point number.");
 
-int main(int argc, char **argv) {
-	log_info("My name: %s", MY_NAME);
+	printf("----- RESULTS -----\n");
 
-	set_age(100);
+	printf("First Name: %s", you.first_name);
+	printf("Last Name: %s", you.last_name);
+	printf("Age: %d\n", you.age);
+	printf("Eyes: %s\n", EYE_COLOR_NAMES[you.eyes]);
+	printf("Income: %f\n", you.income);
 
-	log_info("My age is now: %d", get_age());
+	return 0;
 
-	log_info("THE SIZE is: %d", THE_SIZE);
-	print_size();
-
-	THE_SIZE = 9;
-
-	log_info("THE SIZE is: %d", THE_SIZE);
-	print_size();
-
-	log_info("Ratio at first: %f", update_ratio(2.0));
-	log_info("Ratio again: %f", update_ratio(10.0));
-	log_info("Ratio once more: %f", update_ratio(300.0));
-
-	int count = 4;
-	scope_demo(count);
-	scope_demo(count * 20);
-
-	log_info("count after calling scope_demo: %d", count);
-
-	return 0;	
+error:
+	return -1;
 }
