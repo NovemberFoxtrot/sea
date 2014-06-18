1c991930b144665af734603530a3737d07725749
NovemberFoxtrot <november@fox.trot>
2014-06-17 18:55:21 -0700

    rmf

--- src/main.c
+++ src/main.c
@@ -40,64 +40,11 @@ char *test_dfs() {
 	return NULL;
 }
 
-void lake_counter(char lakes[9][12], int h, int w, int x, int y) {
-	lakes[x][y] = '.';
-
-	for (int dx = -1; dx <= 1; dx++) {
-		for (int dy = -1; dy <= 1; dy++) {
-			int nx = x + dx;
-			int ny = y + dy;
-
-			if (0 >= nx && nx < h && 0 >= ny && ny < w && lakes[y][x] == 'W') {
-				lake_counter(lakes, h, w, nx, ny);
-			}
-		}
-	}
-}
-
-int lake_count(char lakes[9][12], int h, int w) {
-	int res = 0;
-
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < w; j++) {
-			if (lakes[i][j] == 'W') {
-				lake_counter(&lakes, h, w, i, j);
-				res += 1;
-			}
-		}
-	}
-
-	return res;
-}
-
-char *test_lake_counting() {
-	int h = 9;
-	int w = 11;
-
-	char lakes[9][12] = {
-				{ "W........WW." },
-				{ ".WWW.....WWW" },
-				{ "....WW...WW." },
-				{ ".........WW." },
-				{ ".........W.." },
-				{ "..W......W.." },
-				{ ".W.W.....WW." },
-				{ "W.W.W.....W." },
-				{ ".W.W......W." },
-				{ "..W.......W." },
-	};
-
-	printf("lakes: %d\n", lake_count(&lakes, h, w));
-
-	return NULL;
-}
-
 char *all_tests()
 {
 	mu_suite_start();
 
 	mu_run_test(test_dfs);
-	mu_run_test(test_lake_counting);
 
 	return NULL;
 }
