082818547c4cc17dfab0f752f45740b19b0fa9f3
November Foxtrot <november@fox.trot>
2014-06-21 19:59:46 +0900

    t

--- src/main.c
+++ src/main.c
@@ -131,11 +131,104 @@ error:
 	return NULL;
 }
 
-char *all_tests() {
+static DList *dlist = NULL;
+char *test1 = "test1 data";
+char *test2 = "test2 data";
+char *test3 = "test3 data";
+
+char *test_create()
+{
+	dlist = DList_create();
+	mu_assert(dlist != NULL, "Failed to create list.");
+
+	return NULL;
+}
+
+char *test_destroy()
+{
+	DList_clear_destroy(dlist);
+
+	return NULL;
+}
+
+char *test_push_pop()
+{
+	DList_push(dlist, test1);
+	mu_assert(DList_last(dlist) == test1, "Wrong last value.");
+
+	DList_push(dlist, test2);
+	mu_assert(DList_last(dlist) == test2, "Wrong last value");
+
+	DList_push(dlist, test3);
+	mu_assert(DList_last(dlist) == test3, "Wrong last value.");
+	mu_assert(DList_count(dlist) == 3, "Wrong count on push.");
+
+	char *val = DList_pop(dlist);
+	mu_assert(val == test3, "Wrong value on pop.");
+
+	val = DList_pop(dlist);
+	mu_assert(val == test2, "Wrong value on pop.");
+
+	val = DList_pop(dlist);
+	mu_assert(val == test1, "Wrong value on pop.");
+	mu_assert(DList_count(dlist) == 0, "Wrong count after pop.");
+
+	return NULL;
+}
+
+char *test_unshift()
+{
+	DList_unshift(dlist, test1);
+	mu_assert(DList_first(dlist) == test1, "Wrong first value.");
+
+	DList_unshift(dlist, test2);
+	mu_assert(DList_first(dlist) == test2, "Wrong first value");
+
+	DList_unshift(dlist, test3);
+	mu_assert(DList_first(dlist) == test3, "Wrong last value.");
+	mu_assert(DList_count(dlist) == 3, "Wrong count on unshift.");
+
+	return NULL;
+}
+
+char *test_remove()
+{
+	char *val = DList_remove(dlist, dlist->first->next);
+	mu_assert(val == test2, "Wrong removed element.");
+	mu_assert(DList_count(dlist) == 2, "Wrong count after remove.");
+	mu_assert(DList_first(dlist) == test3, "Wrong first after remove.");
+	mu_assert(DList_last(dlist) == test1, "Wrong last after remove.");
+
+	return NULL;
+}
+
+char *test_shift()
+{
+	mu_assert(DList_count(dlist) != 0, "Wrong count before shift.");
+
+	char *val = DList_shift(dlist);
+	mu_assert(val == test3, "Wrong value on shift.");
+
+	val = DList_shift(dlist);
+	mu_assert(val == test1, "Wrong value on shift.");
+	mu_assert(DList_count(dlist) == 0, "Wrong count after shift.");
+
+	return NULL;
+}
+
+char *all_tests()
+{
 	mu_suite_start();
 
 	mu_run_test(test_2d);
 	mu_run_test(test_maze);
+
+	mu_run_test(test_create);
+	mu_run_test(test_push_pop);
+	mu_run_test(test_unshift);
+	mu_run_test(test_remove);
+	mu_run_test(test_shift);
+	mu_run_test(test_destroy);
 
 	return NULL;
 }
